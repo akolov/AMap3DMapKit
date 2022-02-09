556145c755ff05cffc9637af8e16bb785b754d8c
Alexander Kolov <me@alexkolov.com>
2022-02-09 21:45:36 +0900

    Better check

--- Sources/AMap3DMapKit/AMap3DMapKit.swift
+++ Sources/AMap3DMapKit/AMap3DMapKit.swift
@@ -18,7 +18,7 @@ extension MAMapView {
         methodSignature: (@convention(c) (AnyObject, Selector, String?, String?) -> String?).self,
         hookSignature: (@convention(block) (AnyObject, String?, String?) -> String?).self) {
           store in { me, name, ext in
-            guard name == "AMap" && ext == "bundle" && me.bundleIdentifier != "AMap3DMapKit-AMap3DMapKit-resources" else {
+            guard name == "AMap" && ext == "bundle" && me.bundleIdentifier != Bundle.module.bundleIdentifier else {
               return store.original(me, store.selector, name, ext)
             }
             return Bundle.module.path(forResource: name, ofType: ext)
